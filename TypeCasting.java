8c9d2fa9e94ac91d37a0c37fec9290f47597ee57
Mr. Vishnu Kore <89778178+VishnuKore143@users.noreply.github.com>
2021-12-18 23:49:03 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,21 @@
+package Practices;
+
+public class TypeCasting {
+	public static void main(String[]args) {
+		
+	// Automatic Type Casting
+	int x = 25;
+	int y = x;
+	System.out.println(y);
+	
+	
+	// Manual Type Casting
+	double myDouble = 12.58;
+	int myInt =(int)myDouble;
+	System.out.println(myInt);
+	
+	
+
+	}
+	
+}
